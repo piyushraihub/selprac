6273d9e2edefd0ac9e1b6dd46b93059b5e86a18e
RaiPriyadarshini <piyushraiphp@gmail.com>
2023-06-24 11:26:13 +0530

    code changes in API

--- src/main/java/AllImp/API.java
+++ src/main/java/AllImp/API.java
@@ -5,17 +5,22 @@
 //
 //import files.Payload;
 //import io.restassured.RestAssured;
-//
-//public class API {
-//public static void main(String[] args) {
-//	
-//	RestAssured.baseURI = "https://rahulshettyacademy.com";
-//	String respone = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
-//			.body(Payload.addPlace())
-//			.when().post("maps/api/place/add/json")
-//			.then().log().all().assertThat()
-//			.statusCode(200).body("scope", equalTo("APP")).and().header("server", equalTo("Apache/2.4.41 (Ubuntu)"))
-//			.extract().response().asString();
-//	System.out.println("respone******* : " + respone);
-//}
-//}
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+// API class for all rest apis related integration
+
+public class API {
+  public static void main(String[] args) {
+	
+	RestAssured.baseURI = "https://rahulshettyacademy.com";
+	String respone = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+			.body(Payload.addPlace())
+			.when().post("maps/api/place/add/json")
+		.then().log().all().assertThat()
+			.statusCode(200).body("scope", equalTo("APP")).and().header("server", equalTo("Apache/2.4.41 (Ubuntu)"))
+			.extract().response().asString();
+	System.out.println("respone******* : " + respone);
+  }
+}
